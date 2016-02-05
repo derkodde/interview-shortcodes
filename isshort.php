852cc57d6f54d29487d7a0bbbc21d128fa6efeb5
Sebastian Kotte <Sebastian Kotte>
2016-02-05 16:59:45 +0100

    Version 1.0.1
    zweite

---
+++ isshort.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://derkod.de
+ * @since             0.0.0
+ * @package           isshort
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Interview Styles shortcode
+ * Plugin URI:        http://interviewstyles.derkod.de
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           0.3.0
+ * Author:            Sebastian Kotte
+ * Author URI:        http://derkod.de
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       isshort
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*
+*================================
+* Frontend
+*================================
+*/
+
+function enqueue_isshort_styles() {
+
+	/**
+	 * This function is provided for demonstration purposes only.
+	 *
+	 * An instance of this class should be passed to the run() function
+	 * defined in isshort_Loader as all of the hooks are defined
+	 * in that particular class.
+	 *
+	 * The isshort_Loader will then create the relationship
+	 * between the defined hooks and the functions defined in this
+	 * class.
+	 */
+
+	wp_enqueue_style( 'isshort', plugin_dir_url( __FILE__ ) . 'css/isshort-public.css');
+	wp_enqueue_style('animate',  plugin_dir_url( __FILE__ ) . 'css/animate.min.css');
+	wp_enqueue_style('hover',  plugin_dir_url( __FILE__ ) . 'css/hover.css');
+
+
+	// scripts
+	wp_enqueue_script('wowjs',  plugin_dir_url( __FILE__ ) . 'js/wow.min.js');
+	wp_enqueue_script('main',  plugin_dir_url( __FILE__ ) . 'js/isshort.js', array('jquery') );
+
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_isshort_styles' );
+
+
+/*
+*
+*Add Shortcodes
+*
+*/
+function caption_shortcode($atts, $content=null) {
+    // Attributes
+	 $atts = shortcode_atts( array(
+		 'color' => 'red',
+		 'style' => 'circle',
+		 'text-color'=> 'white',
+		 'size' => '4em',
+		 'font-family' => 'Georgia',
+	 	), $atts );
+    return caption_shortcode_html($atts, $content);
+}
+add_shortcode('cap', 'caption_shortcode');
+
+function caption_shortcode_html($atts, $content){
+    	$styles ="background-color:" .  $atts['color'] . ";color:" .  $atts['text-color'] . ";height:".  $atts['size'] .";width:".  $atts['size'];
+
+    ob_start(); ?>
+	<!-- <div class="clearfix"></div> -->
+    <p><div class="isshort-caption <?php echo $atts['style'];?>" style="<?php echo $styles; ?>">
+        <div class="isshort-inner" style="font-size:<?php echo ($atts['size']/2.4); ?>em;font-family:<?php echo $atts['font-family'];?>"><?php echo $content; ?></div>
+    </div></p>
+
+    <?php return ob_get_clean();
+}
+function question_shortcode( $atts , $content = null ) {
+
+	// Attributes
+	 $atts = shortcode_atts( array(
+		 'color' => '#eee',
+		 'corner' => 'top-left',
+		 'radius' => '5px',
+		 'text-color'=> 'grey',
+		 'animate' => 'pulse',
+	 	), $atts );
+
+return isshort_shortcode_html( $atts, $content) ;
+}
+
+add_shortcode( 'question', 'question_shortcode' );
+
+function answer_shortcode( $atts , $content = null ) {
+
+	// Attributes
+	 $atts = shortcode_atts( array(
+		 'color' => '#DCF8C6',
+		 'corner' => 'bottom-right',
+		 'radius' => '5px',
+		 'text-color'=> 'grey',
+		 'animate' => 'pulse',
+	 	), $atts );
+
+return isshort_shortcode_html( $atts, $content) ;
+}
+add_shortcode( 'answer', 'answer_shortcode' );
+
+function isshort_shortcode_html( $atts, $content) {
+	$color=$atts['color'];
+	if ($atts['corner']=='top-left') {
+		$tip='transparent '.$color.' transparent transparent;';
+		// $wow_class = 'rotateInUpLeft';
+	}
+	if ($atts['corner']=='bottom-left'){
+		$tip='transparent transparent '.$color.' transparent ;';
+		// $wow_class = 'rotateInUpLeft';
+	}
+	if ($atts['corner']=='top-right') {
+		$tip= $color.' transparent transparent transparent;';
+		// $wow_class = 'rotateInUpRight';
+	}
+	if ($atts['corner']=='bottom-right') {
+		$tip= 'transparent transparent transparent '.$color;
+		// $wow_class = 'rotateInUpRight';
+	}
+
+	ob_start();
+	?>
+        <div class="isshort-wrapper wow <?php echo $atts['animate']; ?> " data-wow-offset="10">
+            <div class="isshort-side <?php echo $atts['corner']; ?>">
+                <div class="isshort-tip tip-<?php echo $atts['corner']; ?>" style="border-color:<?php echo $tip;?>"></div>
+            </div>
+            <div class="isshort-content content-<?php echo $atts['corner']; ?>" style="border-<?php echo $atts['corner']; ?>-radius:0;color:<?php echo $atts['text-color']; ?>;background-color:<?php echo $atts['color']; ?>;">
+                <?php echo $content; ?>
+            </div>
+        </div>
+        <?php
+	return ob_get_clean();
+}
+
+// Backend
---
+++ js/isshort.js
@@ -0,0 +1,6 @@
+     new WOW().init();
+
+jQuery(document).ready(function($){
+    $('.isshort-caption').next().next().append('<div class="clearfix"></div>');
+    $('.isshort-wrapper').addClass('hvr-pop');
+});
