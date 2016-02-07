b5bd7f59010962c592090e2979b55624d848e6ce
derkodde <basti.kotte@gmail.com>
2016-02-07 21:11:51 +0100

    Backendpage style ready
    - bootstrap included
    - get options

---
+++ admin/admin.php
@@ -0,0 +1,90 @@
+<?php
+
+function isshort_adminpage_bootstrap($hook) {
+    if ( 'plugins_page_isshort-options' != $hook ) {
+        return;
+    }
+
+        wp_register_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js');
+        wp_enqueue_script( 'bootstrap-js');
+        wp_register_style('bootstrap-admin', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
+        wp_enqueue_style('bootstrap-admin');
+        wp_register_style('isshort-admin',  plugin_dir_url( __FILE__ ) . '/admin.css' , array('bootstrap-admin'));
+        wp_enqueue_style('isshort-admin', array('bootstrap-admin'));
+}
+add_action( 'admin_enqueue_scripts', 'isshort_adminpage_bootstrap' );
+
+function isshort_adminpage() {
+
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+    // variables for the field and option names
+    $option_name = 'issc_caption_bgcolor';
+    $hidden_field_name = 'mt_submit_hidden';
+    $data_field_name = 'isshort_cap_color';
+
+    // Read in existing option value from database
+    $issc_caption_bgcolor = get_option(  $option_name, $cap_color );
+
+    $isshort_options = array (
+        'cap' => array (
+        'color' =>  $issc_caption_bgcolor,
+        ),
+    );
+
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $cap_color_val = $_POST[ $data_field_name ];
+
+        // Save the posted value in the database
+        update_option( $option_name, $cap_color_val );
+
+        // Put a "settings saved" message on the screen
+
+?>
+<div class="updated"><p><strong>gespeichert</strong></p></div>
+<?php
+
+    }
+
+    // Now display the settings editing screen
+
+    echo '<div class="wrap ">';
+
+    // header
+    ?>
+    <h2>Interview Styles Shortcode - Options</h2>
+        <h3>Defaults<h3>
+            <h4>Captions <code>[cap]t[/cap]</code></h4>
+            <p>Background color: <code>color="<?php echo $issc_caption_bgcolor; ?>"</code></p>
+
+            <hr />
+        <h3>Settings</h3>
+    <form class="form-horizontal" name="isshort_options" method="post" action="">
+        <div class="form-group ">
+            <h4>Captions</h4>
+            <input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+            <label class="col-sm-2 control-label" for="<?php echo $data_field_name; ?>">Background - Color:</label>
+             <div class="col-sm-6 ">
+                <input class="form-control" type="text"  name="<?php echo $data_field_name; ?>" value="<?php echo $issc_caption_bgcolor; ?>" >
+            </div>
+            <div class="col-sm-4">
+                <code>[cap color="<?php echo $issc_caption_bgcolor; ?>"]S.K.[/cap]</code>
+            </div>
+
+            <hr />
+
+        </div><!--form-group-->
+
+            <p class="submit">
+                <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+            </p>
+    </form>
+</div><!--wrap-->
+<?php
+    return $isshort_options;
+}
